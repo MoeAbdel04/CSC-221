39c6b65240eaa36541522e261df2db1a580da1ac
MoeAbdel04 <145929908+MoeAbdel04@users.noreply.github.com>
2024-09-28 19:56:28 -0400

    debugeed files, now will test

--- M3/M3Lab1/contact_program.py
+++ M3/M3Lab1/contact_program.py
@@ -1,123 +1,61 @@
 
 
 
-#Create the subclass Contact
-#Initialize new attributes for the class
-#Define setters and getters
-#Create the menu 
-#Set up a try catch to Read the csv file content and create instances
-#Write contact information into a txt file  and display contact text file to the user
 
+
+
+
+
+
+# Create the subclass Contact
+# Initialize new attributes for the class
+# Define setters and getters
+# Create the menu 
+# Set up a try-catch to read the CSV file content and create instances
+# Write contact information into a txt file and display contact text file to the user
 
 import con_func as fn
-from m2Lab_clases import Customer
-
-import panda as pd
-  
+from m2Lab_classes import Customer
+import pandas as pd
 
 def main():
-    
-    # call function that reads csv file and creates instances
-    
-    customers =fun.get_cusInfo() # list references Customer instances
-    
-    
-    #Create the main loop which will keep the program running until the user exits
+    # Call function that reads CSV file and creates instances
+    customers = fn.get_cusInfo()  # List references Customer instances
+
+    # Create the main loop which will keep the program running until the user exits
     choice = 0
-    while choice != 4: # not exit
-            
-            # display menu
-            fun.menu()
+    while choice != '4':  # Not exit
+        # Display menu
+        fn.menu()
+
+        choice = input("Please enter an option > ")
         
-
-            choice = input("Please enter an option >")
+        if choice == '1':  # Display instances
+            # Read instances from list and display 
+            for cus in customers:
+                print(cus)
+        
+        elif choice == '2':  # Add customer
+            first = input("Enter first name: ")
+            last = input("Enter last name: ")
+            phone = input("Enter phone: ")
+            email = input("Enter email: ")
+            state = input("Enter state: ")
+            address = input("Enter address: ")
             
-            if choice == 1: # display instances
-                
-                # read instaces from list and display 
-                
-                for cus in customers:
-                    
-                    print(customer)
-
-            elif choice == 2: # add customer
-                
-                # Ask for number of customers to be added
-                num = float(input("How many customers would you like to add? "))
-                
-                # get info
-                
-                for cus_num in range(1,num):
-                    
-                    print("\nCustomer",cus_num,"Info:")
-                    
-                    first = input("Enter first name for customer# ",cus_num," : ")
-                    last = input("Enter last name for customer# ",cus_num," : ")
-                    phone = input("What is ",first+," "+last,"\'s phone number? ")
-                    state = input("In what state does ",first," ",last," live? ")
-                    address = input("Enter ",first," ",last,"\'s address? ")
-                    
-                    # create instance 
-                    customer = Customers(first,last,phone,address,state)
-                    
-                    # add to cumstomers list
-                    
-                customers.append(customer)
-                    
-                # Notify that customer has been added
-                    
-                print("\nCustomer Instance Created")
+            new_customer = Customer(first, last, phone, email, state, address)
+            customers.append(new_customer)
+            print(f"Customer {first} {last} added.")
+        
+        elif choice == '3':  # Update customer info (simplified for demo)
+            print("Update customer feature not implemented.")
+        
+        elif choice == '4':
+            print("Exiting and saving customer data...")
+            # You can implement file saving here
             
-            elif choice == 3: # update custoemr info
-                
-                # ask user for customer last name
-                
-                last = input("Enter customer's last name: ")
-                
-                
-                #call the update function
-                customers = con_func.cus_update(last)
-                
-    
-            elif choice == 4: # Create reports and exit
-                
-                # create new csv file of customers
-                
-                # first create a dataFrame from list of instances
-                
-                # Extract data from instances
-                
-            
-                data = [{
-                    'FirstName': cus.first,
-                    'lastName': cus.last,
-                    'Phone#': cus.phone,
-                    'State': cus.state,
-                    'Address': cus.address
-                    } for cus in customer]
-                
-                # create the dataFrame
-                customerPD = pd.DataFrame(data)
-                # now write to csv
-                
-                customerPD.to_csv("customers_updated",  index=False)
-                #notify user
-                
-                print("\ncustomers_updated.csv has been created(contains updated customer info)")
-                
-                
-                print("\nClosing program! Good bye.")
+        else:
+            print("Invalid option. Please try again.")
 
-                else:
-                    # if anything other than 1 - 4 is inputed give an error
-                    print("This is not a valid option! Please choose from the menu. /n\n")
-            
-
-    
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
-
-
-
-
-
--- M3/M3Lab1/m2Lab_classes.py
+++ M3/M3Lab1/m2Lab_classes.py
@@ -1,66 +1,65 @@
 
+
+
+
+
+
 class Person:
-    def __init__(first, last, phone):
+    def __init__(self, first, last, phone):
         self.__first = first
-        self._last = last
+        self.__last = last
         self.__phone = phone
 
-    def set_first(name):
+    # Setters
+    def set_first(self, name):
         self.__first = name
 
-    def get_last(last):
+    def set_last(self, last):
         self.__last = last
 
-    def set_phone(phone):
+    def set_phone(self, phone):
         self.__phone = phone
-    
+
+    # Getters
     def get_first(self):
         return self.__first
-        
+
     def get_last(self):
-        return self.last
-    
+        return self.__last
+
     def get_phone(self):
-        return self.phone
-    
+        return self.__phone
+
     def __repr__(self):
+        return f'{self.__first:<20}{self.__last:<20}{self.__phone:<20}'
 
-        return f'{first:<20}{last:<20}{get_phone()<20}'
 
-class Customer(self.Person):
-    def __init__(first, last,phone, email, state, address):
-        
-        self.__first = first
-        self.__last = last
+class Customer(Person):
+    def __init__(self, first, last, phone, email, state, address):
+        super().__init__(first, last, phone)
         self.__email = email
-        self._state = state
-        self._address = address
- 
-     
-    #Setters
-    def set_email(self):
-        self.__email = email
-    def set_address(self,address):
-        self._address = address
-    def set_state(state):
         self.__state = state
-  
-    #Getters   
-    def get_email():
-        return email
+        self.__address = address
 
-    def get_address():
-        return self._address
-    
+    # Setters
+    def set_email(self, email):
+        self.__email = email
+
+    def set_address(self, address):
+        self.__address = address
+
+    def set_state(self, state):
+        self.__state = state
+
+    # Getters
+    def get_email(self):
+        return self.__email
+
+    def get_address(self):
+        return self.__address
+
     def get_state(self):
         return self.__state
-    
+
     def __repr__(self):
-
-        return Person._repr_()+f'{self.get_address():<20}{self.get_state()}'
-  
-        
-
-
-
-
+        return f'{super().__repr__()} {self.__email:<20}{self.__address:<30}{self.__state:<10}'
